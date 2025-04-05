d14b9fa877149dfcf2496f024111a3429a279974
Anton <frameofevil@gmail.com>
2025-04-05 15:20:30 +0700

    11

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,12 +29,16 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+            Route::prefix('api')
+                ->middleware('api')
+                ->group(function () {
+                    require base_path('routes/api.php');
+                });
 
             Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+                ->group(function () {
+                    require base_path('routes/web.php');
+                });
         });
     }
 } 
