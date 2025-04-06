19d152475cb55e8f56514829164facedd05e0692
Anton <frameofevil@gmail.com>
2025-04-06 13:35:40 +0700

    dbseed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
             CountrySeeder::class,
             TagSeeder::class,
             RecipeSeeder::class,
