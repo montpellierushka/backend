453fdc443b53c2d32de8d87abde7ea85d8a5b15a
Anton <frameofevil@gmail.com>
2025-04-06 14:09:12 +0700

    t3

--- database/seeders/RouteSeeder.php
+++ database/seeders/RouteSeeder.php
@@ -15,36 +15,43 @@ class RouteSeeder extends Seeder
     public function run(): void
     {
         $user = User::first();
-        $countries = Country::all();
 
         $routes = [
             [
-                'title' => 'Средиземноморский гастротур',
+                'name' => 'Средиземноморский гастротур',
                 'description' => 'Путешествие по средиземноморской кухне: от Италии до Греции',
-                'duration' => 14,
-                'countries' => ['IT', 'GR', 'ES']
+                'countries' => [
+                    ['id' => 1, 'order' => 0], // Италия
+                    ['id' => 2, 'order' => 1], // Греция
+                ],
             ],
             [
-                'title' => 'Азиатский кулинарный тур',
-                'description' => 'Знакомство с разнообразной азиатской кухней',
-                'duration' => 21,
-                'countries' => ['JP', 'CN', 'TH']
-            ]
+                'name' => 'Азиатский кулинарный тур',
+                'description' => 'Знакомство с кухнями Азии: от Японии до Таиланда',
+                'countries' => [
+                    ['id' => 3, 'order' => 0], // Япония
+                    ['id' => 4, 'order' => 1], // Таиланд
+                ],
+            ],
+            [
+                'name' => 'Европейский гастротур',
+                'description' => 'Путешествие по лучшим ресторанам Европы',
+                'countries' => [
+                    ['id' => 5, 'order' => 0], // Франция
+                    ['id' => 6, 'order' => 1], // Испания
+                ],
+            ],
         ];
 
         foreach ($routes as $routeData) {
             $route = Route::create([
-                'title' => $routeData['title'],
+                'name' => $routeData['name'],
                 'description' => $routeData['description'],
-                'duration' => $routeData['duration'],
-                'user_id' => $user->id
+                'user_id' => $user->id,
             ]);
 
-            foreach ($routeData['countries'] as $countryCode) {
-                $country = $countries->where('code', $countryCode)->first();
-                if ($country) {
-                    $route->countries()->attach($country->id, ['order' => array_search($countryCode, $routeData['countries'])]);
-                }
+            foreach ($routeData['countries'] as $country) {
+                $route->countries()->attach($country['id'], ['order' => $country['order']]);
             }
         }
     }
