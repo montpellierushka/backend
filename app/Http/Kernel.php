b650d90341fc92fd89675774c9e6a61e23c66a5f
Anton <frameofevil@gmail.com>
2025-04-07 16:03:06 +0700

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,11 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'telegram.webapp' => \App\Http\Middleware\TelegramWebAppMiddleware::class,
-    ];
-
-    protected $routeMiddleware = [
-        // ... existing middleware ...
         'telegram.webapp' => \App\Http\Middleware\TelegramWebAppAuth::class,
     ];
 } 
