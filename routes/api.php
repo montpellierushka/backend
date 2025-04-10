1f19eb9cb4888082118a39f6078765d57efc7d52
Anton <frameofevil@gmail.com>
2025-04-10 21:43:12 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -75,6 +75,7 @@ Route::post('/upload/image', [UploadController::class, 'image']);
 Route::delete('/upload/{path}', [UploadController::class, 'delete']);
 
 // Защищенные маршруты
+Route::middleware('auth:sanctum')->group(function () {
     // Рецепты
     Route::post('/recipes', [RecipeController::class, 'store']);
     Route::put('/recipes/{recipe}', [RecipeController::class, 'update']);
@@ -90,101 +91,7 @@ Route::delete('/upload/{path}', [UploadController::class, 'delete']);
     Route::put('/users/profile', [UserController::class, 'updateProfile']);
     Route::get('/users/{user}/recipes', [UserController::class, 'recipes']);
     Route::get('/users/{user}/routes', [UserController::class, 'routes']);
-
-// Telegram Bot Routes
-Route::prefix('telegram')->group(function () {
-    Route::post('set-webhook', [TelegramController::class, 'setWebhook']);
-    Route::post('webhook', [TelegramController::class, 'webhook']);
-    Route::get('webhook-info', [TelegramController::class, 'webhookInfo']);
 });
-
-
-
-    Route::prefix('recipes')->group(function () {
-        Route::get('/', [RecipeController::class, 'index']);
-        Route::get('/{recipe}', [RecipeController::class, 'show']);
-        Route::get('/tags', [RecipeController::class, 'tags']);
-        Route::post('/', [RecipeController::class, 'store']);
-        Route::put('/{recipe}', [RecipeController::class, 'update']);
-        Route::delete('/{recipe}', [RecipeController::class, 'destroy']);
-        Route::get('/country/{country}', [RecipeController::class, 'byCountry']);
-        Route::get('/tag/{tag}', [RecipeController::class, 'byTag']);
-    });
-
-    // Route Routes
-    Route::prefix('routes')->group(function () {
-        Route::get('/', [RouteController::class, 'index']);
-        Route::get('/{route}', [RouteController::class, 'show']);
-        Route::post('/', [RouteController::class, 'store']);
-        Route::put('/{route}', [RouteController::class, 'update']);
-        Route::delete('/{route}', [RouteController::class, 'destroy']);
-        Route::get('/country/{country}', [RouteController::class, 'byCountry']);
-    });
-
-    // Country Routes
-    Route::prefix('countries')->group(function () {
-        Route::get('/', [CountryController::class, 'index']);
-        Route::get('/{country}', [CountryController::class, 'show']);
-        Route::get('/{country}/recipes', [CountryController::class, 'recipes']);
-        Route::get('/{country}/routes', [CountryController::class, 'routes']);
-    });
-
-    // Tag Routes
-    Route::prefix('tags')->group(function () {
-        Route::get('/', [TagController::class, 'index']);
-        Route::get('/{tag}', [TagController::class, 'show']);
-        Route::get('/{tag}/recipes', [TagController::class, 'recipes']);
-    });
-
-    // Favorite Routes
-    Route::prefix('favorites')->group(function () {
-        Route::get('/', [FavoriteController::class, 'index']);
-        Route::post('/recipes/{recipe}', [FavoriteController::class, 'addRecipe']);
-        Route::delete('/recipes/{recipe}', [FavoriteController::class, 'removeRecipe']);
-        Route::post('/routes/{route}', [FavoriteController::class, 'addRoute']);
-        Route::delete('/routes/{route}', [FavoriteController::class, 'removeRoute']);
-    });
-
-    // User Routes
-    Route::prefix('users')->group(function () {
-        Route::get('/profile', [UserController::class, 'profile']);
-        Route::put('/profile', [UserController::class, 'updateProfile']);
-        Route::get('/{user}/recipes', [UserController::class, 'recipes']);
-        Route::get('/{user}/routes', [UserController::class, 'routes']);
-    });
-
-    // Search Routes
-    Route::prefix('search')->group(function () {
-        Route::get('/', [SearchController::class, 'search']);
-    });
-
-    // Upload Routes
-    Route::prefix('upload')->group(function () {
-        Route::post('/image', [UploadController::class, 'uploadImage']);
-        Route::delete('/{path}', [UploadController::class, 'delete']);
-    });
-
-    // Stats Routes
-    Route::prefix('stats')->group(function () {
-        Route::get('/', [StatsController::class, 'index']);
-        Route::get('/countries', [StatsController::class, 'countries']);
-        Route::get('/recipes', [StatsController::class, 'recipes']);
-        Route::get('/routes', [StatsController::class, 'routes']);
-    });
-
-    // Comment Routes
-    Route::prefix('comments')->group(function () {
-        Route::get('/recipe/{recipe}', [CommentController::class, 'index']);
-        Route::post('/recipe/{recipe}', [CommentController::class, 'store']);
-        Route::put('/{comment}', [CommentController::class, 'update']);
-        Route::delete('/{comment}', [CommentController::class, 'destroy']);
-    });
-
-    // Like Routes
-    Route::prefix('likes')->group(function () {
-        Route::post('/recipe/{recipe}', [LikeController::class, 'store']);
-        Route::delete('/recipe/{recipe}', [LikeController::class, 'destroy']);
-    });
 
 // Маршруты для карты
 Route::prefix('map')->group(function () {
